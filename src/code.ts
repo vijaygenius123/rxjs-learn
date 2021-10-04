63f454788ca4b187713fe071e22d05503ba9ae9d
Vijayaraghavan Sundararaman <vijaygenius123@gmail.com>
2021-10-04 07:41:56 +0100

    Add Observer Subscribe To Events

--- src/code.ts
+++ src/code.ts
@@ -1,11 +1,25 @@
 import {Observable} from 'rxjs'
 
-var observable = Observable.create((observer: any) => {
+const observable = Observable.create((observer: any) => {
     observer.next('Hey')
+    observer.next('How Are You?')
+    setInterval(() => {
+        observer.next('I Am Good')
+    }, 2000)
+    // observer.complete()
+    // observer.next('???')
 })
 
 
-observable.subscribe((x: any) =>addItem(x))
+const observer = observable.subscribe(
+    (x: any) => addItem(x),
+    (error: any) => addItem(error),
+    () => addItem('Complete')
+)
+
+setTimeout(() => {
+    observer.unsubscribe()
+}, 6000)
 
 
 function addItem(val: any) {
@@ -13,5 +27,4 @@ function addItem(val: any) {
     const textNode = document.createTextNode(val);
     node.appendChild(textNode)
     document.getElementById('output').appendChild(node)
-
 }
